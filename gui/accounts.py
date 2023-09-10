b1030bdbbfcdfeedd5e7770a8f8f5c8b2f195b9a
Stanley Skarshaug <stanley.skarshaug@gmail.com>
2023-09-10 19:00:19 +0200

    fix(gui-accounts): clean up table

--- gui/accounts.py
+++ gui/accounts.py
@@ -11,26 +11,30 @@ class Tab:
         accountsTab = ttk.Frame(tab)
         accountsTab.pack(side="top", expand=1, fill="both")
 
-        tab_label = ttk.Label(
-            accountsTab,
-            text="All accounts"
-        )
-        tab_label.pack(side="top", fill="x", padx=10, pady=6)
+        accounts_db = db.accounts
+        accounts_list = accounts_db.get_all()
 
         table_headlines = ("ID", "Name", "Balance")
         table = ttk.Treeview(
             tab,
             columns=table_headlines,
             show="headings",
-            height=7
+            height=len(accounts_list),
         )
         for i in range(len(table_headlines)):
-            table.column(table_headlines[i], anchor="w")
-            table.heading(table_headlines[i], text=table_headlines[i],anchor="w")
+            if i == 2:
+                anchor = "e"
+            else:
+                anchor = "w"
+            
+            table.column(table_headlines[i], anchor=anchor)
+            table.heading(
+                table_headlines[i], 
+                text=table_headlines[i],
+                anchor=anchor,
+            )
         table.pack()
 
-        accounts_db = db.accounts
-        accounts_list = accounts_db.get_all()
         for row in accounts_list:
             row['balance'] = db.transactions.get_balance(int(row['id']))
             print(row)
